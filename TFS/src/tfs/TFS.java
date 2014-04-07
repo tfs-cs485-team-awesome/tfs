419a98436363f903a8c2f68eecede01086bb120e
Laurence Wong <laurencerwong@gmail.com>
2014-04-06 18:44:36 -0700

    Moved servermaster main function into TFS

--- TFS/src/tfs/TFS.java
+++ TFS/src/tfs/TFS.java
@@ -3,8 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package tfs;
+
+import java.io.*;
+import java.net.*;
 
 /**
  *
@@ -16,7 +18,24 @@ public class TFS {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        String clientSentence;
+        String capitalizedSentence;
+        try {
+            ServerSocket welcomeSocket = new ServerSocket(6789);
+
+            while (true) {
+                Socket connectionSocket = welcomeSocket.accept();
+                BufferedReader inFromClient
+                        = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+                DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+                clientSentence = inFromClient.readLine();
+                System.out.println("Received: " + clientSentence);
+                capitalizedSentence = clientSentence.toUpperCase() + '\n';
+                outToClient.writeBytes(capitalizedSentence);
+            }
+        } catch (Exception e) {
+        }
     }
-    
+
 }
