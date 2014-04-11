833b3ea618b388d0386686ac8b19ba78a16ab3fd
Laurence Wong <laurencerwong@gmail.com>
2014-04-11 04:19:05 -0700

    Added cofnig to TFS

--- TFS/src/tfs/TFS.java
+++ TFS/src/tfs/TFS.java
@@ -7,9 +7,10 @@ package tfs;
 
 import java.io.*;
 import java.net.*;
-import tfs.src.servermaster.ServerMaster;
-import tfs.src.client.Client;
+import java.util.ArrayList;
 import tfs.src.chunkserver.ChunkServer;
+import tfs.src.client.Client;
+import tfs.src.servermaster.ServerMaster;
 
 /**
  *
@@ -22,44 +23,71 @@ public class TFS {
      */
     public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.println("Needs a command line parameter");
-        } else {
-            switch (args[0]) {
-                case "Server":
-                case "server":
-                    ServerMaster server;
-                    if (args.length == 2) {
-                        server = new ServerMaster(Integer.valueOf(args[1]));
-                    } else {
-                        server = new ServerMaster();
+            File f = new File("config.cfg");
+            if (f.exists()) {
+                try {
+                    ArrayList<String> readInArgs = new ArrayList<>();
+                    BufferedReader inFromConfig = new BufferedReader(new FileReader(f));
+                    String readIn = inFromConfig.readLine();
+                    while(readIn != null) {
+                        readInArgs.add(readIn);
+                        readIn = inFromConfig.readLine();
                     }
-
-                    server.RunLoop();
-                    break;
-                case "Client":
-                case "client":
-                    if (args.length == 2) {
-                        Client client = new Client(args[1]);
-                        client.RunLoop();
-                    } else {
-                        System.out.println("Need to specify address and port for client");
-                        return;
-                    }
-                    break;
-                case "Chunk":
-                case "ChunkServer":
-                case "chunk":
-                case "chunkserver":
-                    if (args.length == 3) {
-                        ChunkServer chunk = new ChunkServer(args[1], args[2]);
-                        chunk.RunLoop();
-                    }
-                    break;
-                default:
-                    System.out.println("Invalid argument");
-                    break;
-
+                    System.out.println("Successfully read config.cfg");
+                    String[] inargs = new String[readInArgs.size()];
+                    inargs = readInArgs.toArray(inargs);
+                    start(inargs);
+                } catch (FileNotFoundException fnfe) {
+                    System.out.println("Config file not found");
+                    System.out.println(fnfe.getMessage());
+                } catch (IOException ioe) {
+                    System.out.println("Problem reading file");
+                    System.out.println(ioe.getMessage());
+                }
+            } else {
+                System.out.println("Needs a command line parameter or config file");
             }
+        } else {
+            start(args);
+        }
+    }
+
+    public static void start(String[] args) {
+        switch (args[0]) {
+            case "Server":
+            case "server":
+                ServerMaster server;
+                if (args.length == 2) {
+                    server = new ServerMaster(Integer.valueOf(args[1]));
+                } else {
+                    server = new ServerMaster();
+                }
+
+                server.RunLoop();
+                break;
+            case "Client":
+            case "client":
+                if (args.length == 2) {
+                    Client client = new Client(args[1]);
+                    client.RunLoop();
+                } else {
+                    System.out.println("Need to specify address and port for client");
+                    return;
+                }
+                break;
+            case "Chunk":
+            case "ChunkServer":
+            case "chunk":
+            case "chunkserver":
+                if (args.length == 3) {
+                    ChunkServer chunk = new ChunkServer(args[1], args[2]);
+                    chunk.RunLoop();
+                }
+                break;
+            default:
+                System.out.println("Invalid type");
+                break;
+
         }
     }
 }
