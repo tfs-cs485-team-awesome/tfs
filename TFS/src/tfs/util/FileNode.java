63a9af816694e816614051e78ca893c006a5877e
nprakash <nprakash@Navya-PC>
2014-04-26 23:26:46 -0700

    Added javadoc for filenode

--- TFS/src/tfs/util/FileNode.java
+++ TFS/src/tfs/util/FileNode.java
@@ -24,7 +24,9 @@ public class FileNode {
             mFileMetadata = new FileMetadata();
         }
     }
-
+    /**
+     * Contains all metadata for a chunk.
+     */
     public class ChunkMetadata {
 
         ArrayList<String> mChunkReplicaLocations;
@@ -54,7 +56,10 @@ public class FileNode {
         public long GetTimestamp() {
             return mTimestamp;
         }
-
+        /**
+         * Removes chunk from given location. If chunk is primary location, one of the replica locations will be set to primary.
+         * @param inLocation 
+         */
         public void RemoveChunkLocation(String inLocation) {
             if (inLocation.equalsIgnoreCase(mChunkLocation)) {
                 System.out.println("Removing primary location");
@@ -68,7 +73,10 @@ public class FileNode {
                 RemoveReplicaLocation(inLocation);
             }
         }
-
+        /**
+         * Removes all replica locations for a given chunk.
+         * @param inLocation 
+         */
         public void RemoveReplicaLocation(String inLocation) {
             String foundLocation = null;
             for (String s : mChunkReplicaLocations) {
@@ -82,7 +90,10 @@ public class FileNode {
                 mChunkReplicaLocations.remove(foundLocation);
             }
         }
-
+        /**
+         * If a chunk does not have a primary location, sets the primary location as given location, otherwise adds a replica location.
+         * @param inLocation 
+         */
         public void SetChunkLocation(String inLocation) {
             if (mChunkLocation != null) {
                 //it has a primary location
@@ -92,7 +103,10 @@ public class FileNode {
             System.out.println("Setting chunk's main location");
             mChunkLocation = inLocation;
         }
-
+        /**
+         * Adds a replica location for a chunk if that replica does not already exist.
+         * @param inLocation 
+         */
         public void AddReplicaLocation(String inLocation) {
             for (String s : mChunkReplicaLocations) {
                 if (s.equalsIgnoreCase(inLocation)) {
@@ -120,7 +134,11 @@ public class FileNode {
             return mChunks.size();
         }
     }
-
+    /**
+     * Checks if the given index is invalid, if not, returns the chunk location at that index
+     * @param inIndex
+     * @return string containing the chunk's location
+     */
     public String GetChunkLocationAtIndex(int inIndex) {
         if (inIndex > mFileMetadata.mChunks.size() || inIndex < 0) {
             System.out.println("Attempted to get chunk location at invalid index: " + inIndex);
@@ -129,7 +147,12 @@ public class FileNode {
         }
         return mFileMetadata.mChunks.get(inIndex).mChunkLocation;
     }
-
+    /**
+     * Checks if a chunk exists at a given location
+     * @param inIndex chunk index
+     * @param inLocation location to check for chunk
+     * @return true or false if the chunk exists
+     */
     public boolean DoesChunkExistAtLocation(int inIndex, String inLocation) {
         ChunkMetadata chunkAtIndex = mFileMetadata.mChunks.get(inIndex);
         if (chunkAtIndex.GetPrimaryLocation().equalsIgnoreCase(inLocation)) {
@@ -142,7 +165,11 @@ public class FileNode {
         }
         return false;
     }
-
+    /**
+     * Gets the chunk data at the given index and returns it
+     * @param inIndex
+     * @return Chunk data
+     */
     public ChunkMetadata GetChunkDataAtIndex(int inIndex) {
         if (inIndex >= mFileMetadata.mChunks.size() || inIndex < 0) {
             System.out.println("Attempted to get chunk data at invalid index: " + inIndex);
@@ -150,12 +177,19 @@ public class FileNode {
         }
         return mFileMetadata.mChunks.get(inIndex);
     }
-
+    /**
+     * Adds a chunk at the given location
+     * @param inLocation 
+     */
     public void AddChunkAtLocation(String inLocation) {
         String chunkName = inLocation;
         mFileMetadata.mChunks.add(new ChunkMetadata(chunkName));
     }
-
+    /**
+     * Adds a replica for the given index at the given location
+     * @param inIndex
+     * @param inLocation 
+     */
     public void AddReplicaAtLocation(int inIndex, String inLocation) {
         mFileMetadata.mChunks.get(inIndex).mChunkReplicaLocations.add(inLocation);
     }
